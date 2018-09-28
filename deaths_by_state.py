b7bca345c52172dd38474428f67db36d7a8976f3
rmlassesen <39058137+rmlassesen@users.noreply.github.com>
2018-09-28 10:01:13 +0000

    Update deaths_by_state.py

--- deaths_by_state.py
+++ deaths_by_state.py
@@ -1,12 +1,13 @@
-from datasetter import dataset as dataset
+from datasetter import data as data
 from datasetter import dataset_keys as keys
 
 deaths = {}
+dataset = data.dataset
 
-for i in range(1,53):
-    deaths[i] = sum(dataset[(dataset[:,0] == 2016) & (dataset[:,3] == i)][:,4])
-    
+for i in range(1, 52):
+    deaths[i] = sum(dataset[(dataset[:, 0] == 2016) & (dataset[:, 3] == i)][:, 4])
+
 deaths_by_state_acd = sorted(deaths, key=deaths.__getitem__)
 
-print("State with most deaths(2016):",deaths_by_state_acd[-1])
-print("State with least deaths(2016):",deaths_by_state_acd[0])
+print("State with most deaths(2016):", data.state[deaths_by_state_acd[-1]])
+print("State with least deaths(2016):", data.state[deaths_by_state_acd[1]])
