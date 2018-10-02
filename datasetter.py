2b3b82612edd0f7979522691e3187e1475f68366
rmlassesen <39058137+rmlassesen@users.noreply.github.com>
2018-10-02 02:12:56 +0000

    Update datasetter.py

--- datasetter.py
+++ datasetter.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import urllib.request as req
 
+# create Json_handler class to convert the .json file to csv, and contain indexing keys(icd, cause, state) for future use
 class Json_handler:
     dataset = None
     icd = []
@@ -11,37 +12,48 @@ class Json_handler:
     json = ''
     csv = ''
 
+    # method to generate the dataset for future data-extraction
     def generate(self):
         print("Interpretating ...")
         self.dataset = np.genfromtxt(self.csv, delimiter=',', dtype='uint16')
         print("Done")
+        
 
+    # initialize the class, converting the json-file and creating the index-keys(icd, cause, state)
     def __init__(self, file):
+        # set the filenames
         self.json = file + ".json"
         self.csv = file + ".csv"
 
         print("Converting JSON-file to CSV ...")
+        
+        # open the json file and iterate over the its lines to clean it up a bit
 
         with open(self.json, encoding="UTF8") as fp:
             json_str = ''
             for line in fp:
                 json_str += line
+             # retrieve only the "data"-part of the json, excluding meta-data
             data = json.loads(json_str)
             data = data["data"]
 
             icd = set()
             cause = set()
             state = set()
-
+            
+            # iterate through the datarows, to get a set of states, causes and icd-codes used
             for row in data:
                 icd.add(row[9])
                 cause.add(row[10])
-                state.add(row[11])
+                if row[11] != "United States": # Make sure that "United States" which is not a state is excluded
+                    state.add(row[11])
 
+            # sort the sets alfabetically
             icd = sorted(icd)
             cause = sorted(cause)
             state = sorted(state)
 
+            # convert the sets to a list, with indexes corresponding to the sorted sets
             i = 1
             icd = {key: item for item, key in enumerate(icd, i)}
             i = 1
@@ -49,10 +61,12 @@ class Json_handler:
             i = 1
             state = {key: item for item, key in enumerate(state, i)}
 
+            # if the csv-files has not previously been writen, write it
             if not os.path.isfile(self.csv):
 
                 csv = ''
-                for row in data:
+                #itterate through the data to generate a csv file, making all items comma separated, and adding newline after each row
+                for row in data: 
                     row[9] = icd[row[9]]
                     row[10] = cause[row[10]]
                     if row[11] != "United States":
@@ -62,23 +76,28 @@ class Json_handler:
 
                     csv += ','.join(str(item) for item in row[8:])
                     csv += "\n"
+                    
+                # open/create and write the csv-file
 
-                with open(self.csv, "w") as fw:
+                with open(self.csv, "w") as fw: 
                     fw.write(csv)
             else:
                 print("CSV-file already exists!")
                 print("Meta-data for JSON generated.")
-
-            self.icd = ["United States"] + list(icd)
+            
+            # Create zero-indexes for the data-index-lists
+            self.icd = ["None"] + list(icd)
             self.cause = ["None"] + list(cause)
-            self.state = ["None"] + list(state)
+            self.state = ["United States"] + list(state)
 
         print("Done")
 
 
+# function to download a given url
 def download(url, to=None):
     print("Downloading file ...")
     opener = req.build_opener()
+    # add headers imitating a browser to enforce download permission from all websites
     opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0')]
     req.install_opener(opener)
     req.urlretrieve(url, to)
@@ -87,19 +106,19 @@ def download(url, to=None):
     return True
 
 
-dataset_keys = ["year", "icd", "cause", "state", "deaths", "deaths_age_adj"]
-fileurl = "https://data.cdc.gov/api/views/bi63-dtpu/rows.json?accessType=DOWNLOAD"
-filename = "NCHS_Leading_Causes_of_Death:_US"
+dataset_keys = ["year", "icd", "cause", "state", "deaths", "deaths_age_adj"] #dataset_keys array to use for future data-indexing
+fileurl = "https://data.cdc.gov/api/views/bi63-dtpu/rows.json?accessType=DOWNLOAD" #url to the dataset used
+filename = "NCHS_Leading_Causes_of_Death:_US" #local filename
 
-
+# initialize function downloads and returns the Json_handler-class to be used for data-analysis
 def initialize():
-    if not os.path.isfile(filename + ".json"):
+    if not os.path.isfile(filename + ".json"): #if the json file doesn't exist, download it
         download(fileurl, filename + ".json")
 
-    jhandle = Json_handler(filename)
+    jhandle = Json_handler(filename) # initialize the Json_handler class
 
-    jhandle.generate()
+    jhandle.generate() # generate the dataset
 
-    return jhandle
+    return jhandle # return the class
 
-data = initialize()
+data = initialize() # set data as the initialized Json_handler class
